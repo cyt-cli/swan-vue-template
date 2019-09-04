2ec3f73c96e9f20f629f60d0d42c84ac991759c4
yc111 <yinchuan_111@163.com>
2019-09-04 15:26:45 +0800

    add ask.js

---
+++ ask.js
@@ -0,0 +1,22 @@
+module.exports = [
+    {
+        name: 'private',
+        type: 'confirm',
+        message: 'Is this project private?'
+    },
+    {
+        name: 'author',
+        type: 'input',
+        message: 'author?'
+    },
+    {
+        name: 'description',
+        type: 'input',
+        message: 'description?'
+    },
+    {
+        name: 'license',
+        type: 'input',
+        message: 'license?'
+    }
+]
